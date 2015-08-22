29df746bbbe414c4ccdf527fa64563bf77090d47
Yohei Yamaguchi <joker13meister@gmail.com>
2015-08-22 16:48:19 +0900

    add Podfile and set configs for the build

---
+++ LifeLogMusic/APIClient.swift
@@ -0,0 +1,27 @@
+//
+//  APIClient.swift
+//  LifeLogMusic
+//
+//  Created by Yohei Yamaguchi on 8/22/15.
+//  Copyright (c) 2015 PhysicalEngine. All rights reserved.
+//
+
+import Foundation
+
+class APIClient {
+    
+    static let sharedClient = APIClient()
+    
+    class func timeline() {
+        
+    }
+    
+    class func upload() {
+        
+    }
+    
+    class func download() {
+        
+    }
+    
+}
