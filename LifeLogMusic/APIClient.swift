1f9ed9aaaeb77bcb9a96aa5d0a24d60f91cc29bc
Yohei Yamaguchi <joker13meister@gmail.com>
2015-08-22 20:58:52 +0900

    send the recorded m4a data to the API server

--- LifeLogMusic/APIClient.swift
+++ LifeLogMusic/APIClient.swift
@@ -16,31 +16,44 @@ class APIClient {
     
     static let sharedClient = APIClient()
     
-    private let domain = "https://intense-lake-9534.herokuapp.com"
+    //private let domain = "https://intense-lake-9534.herokuapp.com"
+    private let domain = "http://172.16.200.132:5000"
     
     func timeline(callback:(Result<Timeline, NSError>)->()) {
         Alamofire
             .request(.GET, self.createUrl("list"))
             .responseJSON { (request, response, JSON, error) in
                 if let error = error {
-                    callback(Result(error: error))
+                    return callback(Result(error: error))
                 }
                 if let JSON = JSON {
-                    callback(Result(value: Mapper<Timeline>().map(JSON)!))
+                    return callback(Result(value: Mapper<Timeline>().map(JSON)!))
                 }
             }
     }
 
-    func upload(audio:AnyObject, callback:CallbackType) {
+    func upload(audio:NSData, callback:(Result<AnyObject?, NSError>)->()) {
         Alamofire
-            .request(.POST, self.createUrl("upload"))
-            .responseJSON { (request, response, JSON, error) in
-                if let error = error {
-                    callback(Result(error: error))
-                } else {
-                    callback(Result(value: JSON!))
+        .upload(.POST, URLString: self.createUrl("upload"), headers: nil, multipartFormData: { multipartFormData in
+            multipartFormData.appendBodyPart(data: "gologo".dataUsingEncoding(NSUTF8StringEncoding)!, name: "username")
+            multipartFormData.appendBodyPart(data: audio, name: "file", fileName: "unko.m4a", mimeType: "video/m4a")
+            },
+            encodingMemoryThreshold: Manager.MultipartFormDataEncodingMemoryThreshold,
+            encodingCompletion: { encodingResult in
+                switch encodingResult {
+                case .Success(let upload, _, _):
+                    upload.response { request, response, data, error in
+                        println("request body: \(request.HTTPBody)")
+                        if let error = error {
+                            return callback(Result(error: error))
+                        }
+                        return callback(Result(value: nil))
+                    }
+                case .Failure(let encodingError):
+                    println(encodingError)
                 }
-        }
+            }
+        )
     }
     
     func download(path: String, callback:CallbackType) {
--- LifeLogMusic/RecordingViewController.swift
+++ LifeLogMusic/RecordingViewController.swift
@@ -17,6 +17,9 @@ class RecordingViewController : UIViewController {
     
     // MARK: - IBOutlet/IBAction
     
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
     @IBAction func didTapRecordButton(sender: UIButton) {
         if let recorder = recorder where recorder.recording {
             recorder.pause()
@@ -29,6 +32,27 @@ class RecordingViewController : UIViewController {
     }
     
     @IBAction func didTapStopButton(sender: UIButton) {
+        
+        if recorder == nil {
+            return
+        }
+        
+        println("stop")
+        self.recorder?.stop()
+//        self.meterTimer.invalidate()
+        
+        //self.recordButton.setTitle("Record", forState:.Normal)
+        let session:AVAudioSession = AVAudioSession.sharedInstance()
+        var error: NSError?
+        if !session.setActive(false, error: &error) {
+            println("could not make session inactive")
+            if let e = error {
+                println(e.localizedDescription)
+                return
+            }
+        }
+//        self.stopButton.enabled = false
+//        self.recordButton.enabled = true
     }
     
     @IBOutlet weak var displayLabel: UILabel!
@@ -74,7 +98,6 @@ class RecordingViewController : UIViewController {
     }
     
     func setupRecorder() {
-        
         let dirPaths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask,true)
         var docsDir: AnyObject = dirPaths[0]
         var soundFilePath = docsDir.stringByAppendingPathComponent("Recorded.m4a")
@@ -96,7 +119,6 @@ class RecordingViewController : UIViewController {
             self.recorder?.delegate = self
             self.recorder?.meteringEnabled = true
             self.recorder?.prepareToRecord() // creates/overwrites the file at soundFileURL
-            
         }
     }
     
@@ -105,29 +127,20 @@ class RecordingViewController : UIViewController {
 // MARK: - AVAudioRecorderDelegate
 extension RecordingViewController : AVAudioRecorderDelegate {
     
-    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder!,
-        successfully flag: Bool) {
-            println("finished recording \(flag)")
-            //recordButton.setTitle("Record", forState:.Normal)
-            
-            // iOS8 and later
-            var alert = UIAlertController(title: "Recorder",
-                message: "Finished Recording",
-                preferredStyle: .Alert)
-            alert.addAction(UIAlertAction(title: "Keep", style: .Default, handler: {action in
-                println("keep was tapped")
-            }))
-            alert.addAction(UIAlertAction(title: "Delete", style: .Default, handler: {action in
-                println("delete was tapped")
-                //self.recorder.deleteRecording()
-                self.recorder = nil
-                
-            }))
-            self.presentViewController(alert, animated:true, completion:nil)
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder!, successfully flag: Bool) {
+        println("finished recording \(flag)")
+        
+        recordButton.setTitle("Record", forState:.Normal)
+        
+        var data = NSData(contentsOfURL:recorder.url, options:nil, error:nil)!
+        println("DataSize: \(data.length)")
+
+        APIClient.sharedClient.upload(data, callback: { result in
+            println(result)
+        })
     }
     
-    func audioRecorderEncodeErrorDidOccur(recorder: AVAudioRecorder!,
-        error: NSError!) {
-            println("\(error.localizedDescription)")
+    func audioRecorderEncodeErrorDidOccur(recorder: AVAudioRecorder!, error: NSError!) {
+        println("\(error.localizedDescription)")
     }
 }
