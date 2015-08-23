9d8d0d52b77311d8706a85e3527d84386a4359b3
Yohei Yamaguchi <joker13meister@gmail.com>
2015-08-23 11:31:55 +0900

    bugfix: filter button handling

--- LifeLogMusic/FilterViewController.swift
+++ LifeLogMusic/FilterViewController.swift
@@ -44,7 +44,7 @@ class FilterViewController : UIViewController {
                 SVProgressHUD.dismiss()
                 SVProgressHUD.showSuccessWithStatus("送信完了")
                 
-                let delay = 3.0 * Double(NSEC_PER_SEC)
+                let delay = 1.0 * Double(NSEC_PER_SEC)
                 let time  = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
                 dispatch_after(time, dispatch_get_main_queue(), {
                     self.navigationController?.popToRootViewControllerAnimated(true)
